55d745710ed72da03ad42cdebd79d879c9bdd32c
nmetahri <metahrinafaa@gmail.com>
2024-12-28 13:26:02 +0100

    WIP Config linter + prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,68 @@
+import { fixupConfigRules } from "@eslint/compat";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  {
+    ignores: ["**/dist", "**/.eslintrc.cjs"]
+  },
+  ...fixupConfigRules(
+    compat.extends(
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react/jsx-runtime",
+      "plugin:@typescript-eslint/strict-type-checked",
+      "plugin:@typescript-eslint/stylistic-type-checked",
+      "plugin:react-hooks/recommended",
+      "prettier"
+    )
+  ),
+  {
+    plugins: {
+      "react-refresh": reactRefresh
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      },
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        project: ["./tsconfig.json"],
+        tsconfigRootDir: __dirname
+      }
+    },
+
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true
+        }
+      ]
+    }
+  }
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: "0.0.0.0"
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["src/__tests__/setup.ts"]
   }
-})
+});
